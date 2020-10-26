701710e49b366e620eab8df46d1384001b5eb9f2
HJVeronica <hj.veronica.shim@gmail.com>
2020-10-27 00:16:37 +0900

    "1이 될 때까지" - 2차 풀이
    # 그리디 알고리즘 실전문제 - 1이 될 때까지 (p.99)
    
    해설 확인 후 2차 풀이

--- NDB_CodingTest_Book/Greedy_3/Solution.py
+++ NDB_CodingTest_Book/Greedy_3/Solution.py
@@ -17,13 +17,18 @@ N, K = map(int, input().split())
 result = 0
 
 while True:
-    if N == 1:
+    # N이 K의 배수가 될 때까지 1씩 빼기
+    target = (N//K) * K
+    result += (N - target)
+    N = target
+
+    # N이 K보다 작아져서 더 나눌 수 없을 때 break
+    if N < K:
         break
 
-    if N % K == 0:
-        N /= K
-    else:
-        N -= 1
     result += 1
+    N //= K
 
+# 남은 수에 대해 1씩 빼기
+result += (N - 1)
 print(result)
